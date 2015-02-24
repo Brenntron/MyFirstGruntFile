b5059b525a7fead8c6791fae8f2929af4c8d7e10
Brennan Willingham <brennanw@Brennan-Willinghams-MacBook-Pro.local>
2015-02-24 09:30:03 -0600

    added grunt file with watch task.

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    clean: ['public'],
+    copy: {
+      main:{
+        files: [
+          {expand: true, cwd: 'app/', src: ['**', '!**/*.jade', '!**/*.{scss,sass}'], dest: 'public/', filter: 'isFile'}
+        ]
+      }
+    },
+    jade: {
+      compile: {
+        files: [
+          {expand: true, cwd: 'app/', src: ['**/*.jade', '!**/_*.jade'], dest: 'public/', ext: '.html'}
+        ]
+      }
+    },
+    sass: {
+      options: {
+        sourceMap: true
+        },
+      dist: {
+        files: {
+          'public/css/main.css': 'app/styles/main.scss'
+        }
+      }
+    },
+    watch: {
+      files: ['app/**/*'],
+      tasks: ['build'],
+      options: {
+        reload: true
+      }
+    }
+  });
+
+  grunt.registerTask('default', []);
+  grunt.registerTask('build', ['clean', 'copy', 'jade', 'sass']);
+};
